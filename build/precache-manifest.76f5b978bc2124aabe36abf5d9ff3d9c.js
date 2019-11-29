983afc35d65648bc704fe94a18977c52014825f4
fadlurss <jhieber7@gmail.com>
2019-11-29 13:39:48 +0700

    init

---
+++ build/precache-manifest.76f5b978bc2124aabe36abf5d9ff3d9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d680a376a1f579d96b666bc4d850dca8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d1f45cfcf30376306b8",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6fea7dec174f8202d156",
+    "url": "/static/js/2.4734f653.chunk.js"
+  },
+  {
+    "revision": "7d1f45cfcf30376306b8",
+    "url": "/static/js/main.69535a6a.chunk.js"
+  },
+  {
+    "revision": "4fe42612385b5bbe4a65",
+    "url": "/static/js/runtime-main.a107ab13.js"
+  }
+]);
---
+++ src/Table.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Remove</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = props => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
+            </tr>
+        );
+    });
+    return <tbody>{rows}</tbody>
+}
+
+
+class Table extends Component {
+    render() {
+
+        const { characterData, removeCharacter } = this.props;
+
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+            </table>
+        )
+    }
+}
+
+export default Table;
+
+
+// class Table extends Component {
+//     render() {
+//         return (
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th>Name</th>
+//                         <th>Job</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <td>Ali</td>
+//                         <td>Masak</td>
+//                     </tr>
+//                     <tr>
+//                         <td>Udin</td>
+//                         <td>Juru mesin</td>
+//                     </tr>
+//                     <tr>
+//                         <td>Udin</td>
+//                         <td>Juru mesin</td>
+//                     </tr>
+//                     <tr>
+//                         <td>Udin</td>
+//                         <td>Juru mesin</td>
+//                     </tr>
+//                 </tbody>
+//             </table>
+//         )
+//     }
+// }
+
+
+// export default Table
